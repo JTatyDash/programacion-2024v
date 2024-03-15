2d6c812071464d317d9d6aad35a53001deb20ff2
JTatyDash <jenny021920@gmail.com>
2024-03-14 21:12:44 -0500

    variables y operadores

---
+++ intro/operadores.js
@@ -0,0 +1,22 @@
+// operadores aritmeticos basicos
+
+let counter = 100;
+
+counter = counter + 20;
+console.log(counter);
+counter += 20;
+console.log(counter);
+
+
+let counter1 = 200;
+rest = counter1 - 10;
+console.log(rest);
+rest -= 10;
+console.log(rest);
+
+
+let counter2 = 300;
+div = counter2 / 85;
+console.log(div);
+div /= 10;
+console.log(div);
